17aeb0cf57bf1462cc6df73c9a7e49b436f0d871
islamby <islambekkaparov13@gmail.com>
2024-08-14 15:09:21 +0300

    hwfirst

---
+++ src/Num.jsx
@@ -0,0 +1,13 @@
+import { UseSelector, useSelector } from "react-redux";
+
+const Num = () => {
+    const num = useSelector(s => s.reducer.num)
+    return (
+        <div>
+            <h1>{num}</h1>
+            
+        </div>
+    );
+}
+
+export default Num;
